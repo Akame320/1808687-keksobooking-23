a979a6b92ff74337ceb030244f89554ebad55980
Антон Петров <akame320@yandex.ru>
2021-06-13 15:11:51 +0300

    Внес новую фнукцию и сделал изменения

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-function randomNumber(minNumber, maxNumber, lengthChars = -1){
-  let outNumber;
-
-  if(minNumber < maxNumber){
-    outNumber = Math.random() * (maxNumber - minNumber) + minNumber;
+function precisionRandomNumber(minNumber, maxNumber, precision = -1){
+  if (minNumber < 0 || maxNumber < 0 || maxNumber < minNumber || precision < 0) {
+    throw new Error('Ожидаемые аргументы: 0 < minNumber < maxNumber, 0 < precision');
   }
-
-  if(lengthChars >= 0){
-    outNumber = outNumber.toFixed(lengthChars);
-  }
-
-  return outNumber;
+  return +(Math.random() * (maxNumber - minNumber) + minNumber).toFixed(precision);
 }
 
-randomNumber(0, 2, 2);
+function randomNumber(minNumber, maxNumber) {
+  if (minNumber < 0 || maxNumber < 0 || maxNumber < minNumber) {
+    throw new Error('Ожидаемые аргументы: 0 < minNumber < maxNumber');
+  }
+  return Math.random() * (maxNumber - minNumber) + minNumber;
+}
+
+precisionRandomNumber(0, 100, 2);
+randomNumber(10, 100);
+
