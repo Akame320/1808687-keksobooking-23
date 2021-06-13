cafe42407406b9caa003c805ea0bc84c2d46988c
Антон Петров <akame320@yandex.ru>
2021-06-13 22:28:06 +0300

    Внесение 2 правок

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
-function precisionRandomNumber(minNumber, maxNumber, precision = -1){
+function getRandomFloat(minNumber, maxNumber, precision = -1){
   if (minNumber < 0 || maxNumber < 0 || maxNumber < minNumber || precision < 0) {
     throw new Error('Ожидаемые аргументы: 0 < minNumber < maxNumber, 0 < precision');
   }
   return +(Math.random() * (maxNumber - minNumber) + minNumber).toFixed(precision);
 }
 
-function randomNumber(minNumber, maxNumber) {
-  if (minNumber < 0 || maxNumber < 0 || maxNumber < minNumber) {
-    throw new Error('Ожидаемые аргументы: 0 < minNumber < maxNumber');
-  }
-  return Math.random() * (maxNumber - minNumber) + minNumber;
+function getRandomInt(minNumber, maxNumber) {
+  return getRandomFloat(minNumber, maxNumber, 0);
 }
 
-precisionRandomNumber(0, 100, 2);
-randomNumber(10, 100);
+getRandomFloat(10, 20, 10);
+getRandomInt(10, 100);
 
