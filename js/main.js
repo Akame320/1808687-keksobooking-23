a8412ae74d039e581a1e4dd45033c76edfd844ac
Ruzen <Akame320@yandex.ru>
2021-06-24 20:44:13 +0300

    Больше функций 3

--- js/main.js
+++ js/main.js
@@ -1,88 +1,64 @@
-const avatarLinks = [];
 const OFFER_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const TYPES_CHECKIN = ['12:00', '13:00', '14:00'];
-const TYPES_CHECKOUT = ['12:00', '13:00', '14:00'];
+const TYPES_CHECKS = ['12:00', '13:00', '14:00'];
 const TYPES_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-const allAds = [];
+const LAT_START = 35.65000;
+const LAT_END = 35.70000;
+const LNG_START = 139.70000;
+const LNG_END = 139.80000;
 
-
-function getRandomFloat(minNumber, maxNumber, precision = -1) {
+const getRandomFloat = (minNumber, maxNumber, precision = -1) => {
   if (minNumber < 0 || maxNumber < 0 || maxNumber < minNumber || precision < 0) {
     throw new Error('Ожидаемые аргументы: 0 < minNumber < maxNumber, 0 < precision');
   }
   return +(Math.random() * (maxNumber - minNumber) + minNumber).toFixed(precision);
-}
+};
 
 function getRandomInt(minNumber, maxNumber) {
   return getRandomFloat(minNumber, maxNumber, 0);
 }
 
-function getAvatar() {
-  let randomNumberForAvatar = getRandomInt(0, 10);
-  while (avatarLinks.includes(randomNumberForAvatar)) {
-    randomNumberForAvatar = getRandomInt(0, 10);
-  }
-
-  avatarLinks.push(randomNumberForAvatar);
-
-  if(randomNumberForAvatar <= 9){
+function addLeadingZeroIfNeeded(randomNumberForAvatar) {
+  if (randomNumberForAvatar <= 9) {
     return `img/avatars/user0${randomNumberForAvatar}`;
   }
   return `img/avatars/user${randomNumberForAvatar}`;
 }
 
-function getRandomElementFromArray(array){
+function getRandomElementFromArray(array) {
   return array[getRandomInt(0, array.length - 1)];
 }
 
-function getShuffleArray(array){
-  return array.slice().sort(() => Math.random() -0.5);
+function getShuffledArray(array) {
+  return array.slice().sort(function () { return Math.random() - 0.5; });
 }
 
-//START
-function getPhotos() {
-  const START_END_RANDOM_NUMBER = getRandomInt(1, PHOTOS.length - 1);
-  return PHOTOS.slice(getRandomInt(0, START_END_RANDOM_NUMBER - 1), getRandomInt(START_END_RANDOM_NUMBER, PHOTOS.length - 1));
-}
-//END
-
-function createAds() {
-  const LAT = getRandomFloat(35.65000, 35.70000, 5);
-  const LNG = getRandomFloat(139.70000, 139.80000, 5);
-
-  const PRICE = getRandomInt(0, 500000);
-  const TYPE = getRandomElementFromArray(OFFER_TYPES);
-  const ROOMS = getRandomInt(0, 10);
-  const GUESTS = getRandomInt(0, 1000);
-  const CHECKIN = getRandomElementFromArray(TYPES_CHECKIN);
-  const CHECKOUT = getRandomElementFromArray(TYPES_CHECKOUT);
-  const FEATURES = getShuffleArray(TYPES_FEATURES);
+function generateOffer() {
+  const lat = getRandomFloat(LAT_START, LAT_END, 5);
+  const lng = getRandomFloat(LNG_START, LNG_END, 5);
 
   return {
-    author : {
-      avatar: getAvatar(),
+    author: {
+      avatar: addLeadingZeroIfNeeded(getRandomInt(0, 10)),
     },
-    location : {
-      lat: LAT,
-      lng: LNG,
+    location: {
+      lat: lat,
+      lng: lng,
     },
-    offer : {
+    offer: {
       title: 'Заголовок квартиры',
-      price: PRICE,
-      address: [LAT, LNG],
-      type: TYPE,
-      rooms: ROOMS(0, 10),
-      guests: GUESTS(0, 1000),
-      checkin: CHECKIN(TYPES_CHECKIN),
-      checkout: CHECKOUT,
-      features: FEATURES,
+      price: getRandomInt(0, 500000),
+      address: `${lat},${lng}`,
+      type: getRandomElementFromArray(OFFER_TYPES),
+      rooms: getRandomInt(0, 10),
+      guests: getRandomInt(0, 1000),
+      checkin: getRandomElementFromArray(TYPES_CHECKS),
+      checkout: getRandomElementFromArray(TYPES_CHECKS),
+      features: getShuffledArray(TYPES_FEATURES),
       description: 'Описание квартиры',
-      PHOTOS: getPhotos(),
+      PHOTOS: getShuffledArray(PHOTOS),
     },
   };
 }
 
-for (let i = 0; i < 10; i++) {
-  allAds.push(createAds());
-}
+const offers = new Array(10).fill(null).map(generateOffer);
