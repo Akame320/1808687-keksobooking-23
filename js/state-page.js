99f79e6d6ae09c9ffc916b7193780363394bbbd0
Ruzen <Akame320@yandex.ru>
2021-07-04 19:51:27 +0300

    init commit 2

--- js/state-page.js
+++ js/state-page.js
@@ -5,19 +5,19 @@ const selectMapFilters = document.querySelectorAll('.map__filter');
 
 const setStatePageActive =() => {
   adForm.classList.remove('ad-form--disabled');
-  inputAdForm.forEach(input => input.removeAttribute('disabled'));
+  inputAdForm.forEach((input) => input.removeAttribute('disabled'));
 
   mapFilters.classList.remove('ad-form--disabled');
-  selectMapFilters.forEach(select => select.removeAttribute('disabled'));
-}
+  selectMapFilters.forEach((select) => select.removeAttribute('disabled'));
+};
 
 const setStatePageDisable = () =>{
   adForm.classList.add('ad-form--disabled');
-  inputAdForm.forEach(input => input.setAttribute('disabled', true));
+  inputAdForm.forEach((input) => input.setAttribute('disabled', true));
 
   mapFilters.classList.add('ad-form--disabled');
-  selectMapFilters.forEach(select => select.setAttribute('disabled', true));
-}
+  selectMapFilters.forEach((select) => select.setAttribute('disabled', true));
+};
 
 
 export {setStatePageDisable, setStatePageActive};
