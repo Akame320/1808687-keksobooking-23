ed2b09427dc179e474e1bd2d0fc8870cba3dc26d
Ruzen <Akame320@yandex.ru>
2021-07-09 21:57:41 +0300

    add validate Form

--- js/state-page/form.js
+++ js/state-page/form.js
@@ -11,5 +11,33 @@ const setAdFormEnabled = (enabled) => {
   }
 };
 
+
+const inputCapacity = document.querySelector('#capacity');
+const inputRoom = document.querySelector('#room_number');
+const addOfferSubmitBtn = document.querySelector('#add-offer-submit-btn');
+
+const isValidateCapacity = () => {
+  const maxValueForCapacity = +inputRoom.value;
+
+  for (const child of inputCapacity.children) {
+    if(+child.value > maxValueForCapacity || +child.value === 0){
+      child.setAttribute('disabled', 'disabled');
+    }else{
+      child.removeAttribute('disabled');
+    }
+  }
+};
+
+inputRoom.addEventListener('input', isValidateCapacity);
+
+addOfferSubmitBtn.addEventListener('submit', () => {
+  isValidateCapacity();
+});
+
+window.onload = () => {
+  isValidateCapacity();
+};
+
+
 export {setAdFormEnabled};
 
